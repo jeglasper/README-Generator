87b0bb58d9523d4c315decc5e2d2c7a5407d57d8
jeglasper <109081362+jeglasper@users.noreply.github.com>
2023-03-29 05:32:06 -0500

    create array of questions for user input

--- index.js
+++ index.js
@@ -5,7 +5,50 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "Title: What's your project title?", 
+        name: 'title',
+    }, {
+        type: 'input',
+        message: "Description: What is your motivation and purpose for creating this project?", 
+        name: 'motivation',
+    }, {
+        type: 'input',
+        message: "Description: What did you learn from creating this project?", 
+        name: 'learned',
+    }, {
+        type: 'input',
+        message: "Installation: What are the steps required to install your project?", 
+        name: 'installation',
+    }, {
+        type: 'input',
+        message: "Usage: Provide instructions and examples for use.",
+        name: 'usage',
+    }, {
+        type: 'list',
+        message: "License: What type of licence will you use for this project?", 
+        choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" Licence', 'BSD 3-Clause "New" or "Revised" License', 'Boost Softward License 2.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero GPL v3.0', 'GNU GPL v2.0', 'GNU Lesser GPL v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+        name: 'license'
+    }, {
+        type: 'input',
+        message: 'How to Contribute: Include additional guidelines for how to contribute (beyond the Contributor Covenant).', 
+        name: 'contributing',
+    }, {
+        type: 'input',
+        message: "Tests: What tests did you complete during this project? Provide examples of how to run tests", 
+        name: 'tests'
+    }, {
+        type: 'input',
+        message: 'Questions: What is your GitHub username?',
+        name:  'username',
+    }, {
+        type: 'input',
+        message: 'Questions: What is your email address?',
+        name: 'email',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
